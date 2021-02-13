eefb45d074a98d07d86c6ce01397fa3f5cd4fb19
iLaD08 <imadeddinelakhdar08@gmail.com>
2021-02-13 10:34:13 +0100

    updated deletePost

--- src/actions/deletePost.js
+++ src/actions/deletePost.js
@@ -2,7 +2,11 @@ import API from '../api/api';
 import { toast } from 'react-toastify';
 
 const DeletePost = ({ id }) => {
-    API.delete('post/' + id)
+    API.delete('post/' + id, {
+        headers: {
+            Authorization: localStorage.getItem('token')
+        }
+    })
         .then(() => {
             toast.success('Post Deleted!');
             setTimeout(() => {
