3e8e7466e2ec9ff1482455893a99f556d0a0aaa7
iLaD08 <imadeddinelakhdar08@gmail.com>
2021-02-20 20:36:22 +0100

    Added post title in code-page title

--- src/actions/getPost.js
+++ src/actions/getPost.js
@@ -3,6 +3,7 @@ import API from '../api/api';
 const getPost = ({ id, setPost, setLoading, loading }) => {
     API.get('post/' + id)
         .then(res => {
+            document.title = `ShareCode | ${res.data.title}`;
             setTimeout(() => {
                 setPost(res.data);
                 setLoading(!loading);
