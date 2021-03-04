72fb68a11191858115391ac22923cce1bcea663e
iLaD08 <imadeddinelakhdar08@gmail.com>
2021-03-04 19:16:37 +0100

    added animation to leaderboard-page

--- src/pages/leaderboard-page/style.js
+++ src/pages/leaderboard-page/style.js
@@ -1,9 +1,21 @@
-import styled from 'styled-components';
+import styled, { keyframes } from "styled-components";
+
+const ToRightSlide = keyframes`
+    0% {
+        transform: translateX(-10%);
+        opacity: 0;
+    }
+     100% {
+        transform: translateX(0);
+        opacity: 1;
+    }
+`;
 
 export const LeaderboardDivContainner = styled.div`
     display: flex;
     justify-content: center;
     margin-bottom: 2%;
+    animation: 1s ease-out 0s 1 ${ToRightSlide};
 `;
 
 export const LeaderboardUl = styled.div`
