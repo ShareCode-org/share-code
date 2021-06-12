751cafc76c4ebf25ca1a1f161678b97b07e6f40a
iLaD08 <imadeddinelakhdar08@gmail.com>
2021-06-12 18:00:15 +0100

    removed unused var

--- src/pages/raw-page/index.js
+++ src/pages/raw-page/index.js
@@ -1,27 +1,25 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router";
 import getPost from "../../actions/getPost";
-import IconButton from '@material-ui/core/IconButton';
-import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 
 const RawPage = () => {
-	const { id } = useParams();
-	const [post, setPost] = useState({});
+  const { id } = useParams();
+  const [post, setPost] = useState({});
 
-	useEffect(() => {
-		var load = false;
-		getPost({
-			id,
-			setPost,
-			load,
-		});
-	}, []);
+  useEffect(() => {
+    var load = false;
+    getPost({
+      id,
+      setPost,
+      load,
+    });
+  }, []);
 
-	return (
-		<div style={{ margin: '2%' }}>
-			<pre>{post.code}</pre>
-		</div>
-	);
+  return (
+    <div style={{ margin: "2%" }}>
+      <pre>{post.code}</pre>
+    </div>
+  );
 };
 
 export default RawPage;
