25bfc5c1cc24d913927e2bf767f2c2673691f563
iLaD08 <imadeddinelakhdar08@gmail.com>
2021-06-12 17:58:28 +0100

    edited leaderboard-item

--- src/components/leaderboard-item/index.js
+++ src/components/leaderboard-item/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React from "react";
 
-const LeaderboardItem = ({ Number, Username, postsNumber, followersNumber }) => (
-    <tr>
-        <td>{Number}#</td>
-        <td onClick={() => window.location.href = `/profile/${Username}`}>
-            <span style={{ cursor: 'pointer' }}>{Username}</span>
-        </td>
-        <td>{postsNumber}</td>
-        <td>{followersNumber}</td>
-    </tr>
+const LeaderboardItem = ({
+  Number,
+  Username,
+  postsNumber,
+  followersNumber,
+}) => (
+  <tr>
+    <td>{Number + 1}#</td>
+    <td onClick={() => (window.location.href = `/profile/${Username}`)}>
+      <span style={{ cursor: "pointer" }}>{Username}</span>
+    </td>
+    <td>{postsNumber}</td>
+    <td>{followersNumber}</td>
+  </tr>
 );
 
 export default LeaderboardItem;
